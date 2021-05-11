87eb780c49ba458fefcddbb1fba48a236d306f5c
João Brentano <joaobrentano.br@gmail.com>
2021-05-11 17:11:32 -0300

    Fix

--- CevaSearch/View/MainView.swift
+++ CevaSearch/View/MainView.swift
@@ -15,13 +15,10 @@ struct MainView: View {
             VStack(alignment: .leading) {
                 SearchHeaderView()
                 HeaderUser(userName: "Brentano")
-                TextField("Type your search",text: $searchText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 VStack(alignment: .leading) {
                     Text("7 results founded")
                         .font(.subheadline)
                         .fontWeight(.semibold).multilineTextAlignment(.leading)
-                    
                     List(beers) { beer in
                         NavigationLink(
                             destination: BeerView(beer: beer)
@@ -42,6 +39,5 @@ struct MainView: View {
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
-        
     }
 }
