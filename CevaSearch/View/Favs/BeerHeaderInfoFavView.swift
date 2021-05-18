5f80a4f418cbadfae6d6e1d87431a962c1d337df
Rodrigo Kroef Tarouco <rodrigoktarouco@gmail.com>
2021-05-18 15:06:55 -0300

    Adding part of favorites view

---
+++ CevaSearch/View/Favs/BeerHeaderInfoFavView.swift
@@ -0,0 +1,64 @@
+//
+//  BeerHeaderInfoFavView.swift
+//  CevaSearch
+//
+//  Created by Rodrigo Kroef Tarouco on 17/05/21.
+//
+
+import SwiftUI
+import Foundation
+
+struct BeerHeaderInfoFavView: View {
+    
+    var beer: Beer
+    
+    var body: some View {
+        VStack {
+            HStack {
+                beer.image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .scaledToFit()
+                    .frame(width: 125, height: 125)
+                    .clipped()
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color(white: 0.4))
+                    )
+                    .shadow(radius: 3)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack(alignment: .leading) {
+                    Text("\(beer.name)")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding(.trailing)
+                        .aspectRatio(contentMode: .fill)
+                    Text("\(beer.type)")
+                        .fontWeight(.semibold)
+                        .padding([.top, .trailing])
+                            .aspectRatio(contentMode: .fit)
+                    Divider()
+                    Text("\(beer.origin) - IBU \(String(format: "%.1f", beer.ibu))")
+                        .fontWeight(.semibold)
+                        .padding(.trailing)
+                        .aspectRatio(contentMode: .fill)
+                    
+                }
+                .padding([.bottom, .trailing])
+                
+            }
+        }
+        .aspectRatio(contentMode: .fit)
+        
+        
+        
+    }
+    
+}
+
+struct BeerHeaderInfoFavView_Previews: PreviewProvider {
+    static var previews: some View {
+        BeerHeaderInfoFavView(beer: beers[1])
+    }
+}
--- CevaSearch/View/Main/SearchBarView.swift
+++ CevaSearch/View/Main/SearchBarView.swift
@@ -11,15 +11,20 @@ struct SearchBarView: View {
     
     @State var searchText = ""
     
+    
     var body: some View {
         TextField("🔍 Search a beer for name, IBU, type, color...",text: $searchText)
             .foregroundColor(.gray)
             .textFieldStyle(RoundedBorderTextFieldStyle())
+            .border(Color.black, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/).cornerRadius(11.0)
+            
     }
 }
 
 struct SearchBarView_Previews: PreviewProvider {
     static var previews: some View {
         SearchBarView()
+            .padding(.horizontal)
+            
     }
 }
